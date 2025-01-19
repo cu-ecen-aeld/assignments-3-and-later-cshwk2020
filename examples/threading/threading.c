314f8c41ff299004f56969d770204ee3d5d27aec
Hung Wai Kay <cshwk2020@gmail.com>
2025-01-19 17:40:53 +0800

    hw4_1 trigger runner

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
- 
+
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
